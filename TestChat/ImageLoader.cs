561e3dfa5ea47aa307f114499226e7bc092194b1
Dumitru Daniel Mititelu <mttl_daniel@yahoo.com>
2015-05-04 09:48:51 +0300

    Updated the ImageLoader class.

--- TestChat/ImageLoader.cs
+++ TestChat/ImageLoader.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 
 namespace TestChat {
     class ImageLoader {
-
-        CroppedBitmap[] objImg = new CroppedBitmap[65];
+        public ImageLoader () {
+            CutImage ();
+            LoadImage ();
+        }
+        Image[] image=new Image[106];
+        CroppedBitmap[] objImg=new CroppedBitmap[65];
+        double scale=2;
 
         private void CutImage () {
             int count=0;
@@ -20,9 +26,29 @@ namespace TestChat {
             src.CacheOption=BitmapCacheOption.OnLoad;
             src.EndInit ();
 
-            for (int i=0; i<4; i++)
-                for (int j=0; j<12; j++)
+            for (int i=0; i<5; i++)
+                for (int j=0; j<13; j++)
                     objImg[count++]=new CroppedBitmap (src, new Int32Rect (j*32, i*48, 32, 48));
+        }
+
+        private void LoadImage () {
+            for (int i=0; i<106; i++) {
+                if (i<53) {
+                    image[i]=new Image ();
+                    image[i].Source=objImg[i];
+                    image[i].Width=objImg[i].Width*scale;
+                    image[i].Height=objImg[i].Height*scale;
+                } else {
+                    image[i]=new Image ();
+                    image[i].Source=objImg[i-53];
+                    image[i].Width=objImg[i-53].Width*scale;
+                    image[i].Height=objImg[i-53].Height*scale;
+                }
+            }
+        }
+        public Image[] getImage {
+            get { return image; }
+            set { ;}
         }
     }
 }
